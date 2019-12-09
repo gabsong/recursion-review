7817c159053445e8eb57bcc96822e11ec45f96ec
gabsong <gabsong4@gmail.com>
2019-12-09 11:56:59 -0800

    Start on getElementsByClassName.js
    Started on the first function, no functionality yet.

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,21 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
+var getElementsByClassName = function(className, e) {
   // your code here
+  var arr = [];
+  e = e || document.body;
+
+  if (e.className === className) {
+    arr.push(e);
+  }
+
+  for (let i = 0; i < e.children.length; i++) {
+    // calling getElementsByClassName on children
+    let result = getElementsByClassName(className, e.children[i]);
+    console.log(e.children[i].className, result);
+  }
+
+  // return array with elements
+  return arr;
 };
