43d1e33a192317278fac2f7b8bd2a9505bc2541c
gabsong <gabsong4@gmail.com>
2019-12-09 17:49:24 -0800

    Complete stringifyJSON
    Assignment is complete.

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,38 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  var result = '';
+  if (typeof obj === 'number') {
+    result += obj + '';
+  } else if (obj === null) {
+    result += 'null';
+  } else if (typeof obj === 'boolean') {
+    result += obj;
+  } else if (typeof obj === 'string') {
+    result += '"' + obj + '"';
+  } else if (Array.isArray(obj)) {
+    result += '[';
+    for (let i = 0; i < obj.length; i++) {
+      result += stringifyJSON(obj[i]);
+      if (i !== obj.length - 1) {
+        result += ',';
+      }
+    }
+    result += ']';
+  } else if (typeof obj === 'function') {
+    // do nothing
+  } else if (obj === undefined) {
+    // do nothing
+  } else {
+    result += '{';
+    for (let key in obj) {
+      if (!(obj[key] === undefined || typeof obj[key] === 'function'))
+        result += stringifyJSON(key) + ':' + stringifyJSON(obj[key]) + ',';
+    }
+    if (result.length > 1) {
+      result = result.substring(0, result.length - 1);
+    }
+    result += '}';
+  }
+  return result;
 };
